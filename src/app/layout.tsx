8c25517abfd5aeb11e7516ff94691de090bfbba2
Khang2411 <46453014+Khang2411@users.noreply.github.com>
2024-07-06 03:41:15 +0700

    Update layout.tsx 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const dynamic = 'auto'
 export const dynamicParams = true
 export const revalidate = false
 export const fetchCache = 'auto'
-export const runtime = 'nodejs'
+export const runtime = 'edge'
 export const preferredRegion = 'auto'
 export const maxDuration = 60
 
