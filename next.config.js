dcd8fa2d05e2af503d24ce34f432a53b3f8f324c
Khang2411 <46453014+Khang2411@users.noreply.github.com>
2024-07-05 22:31:51 +0700

    Update next.config.js 1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      NEXTAUTH_URL='https://ktravel.io.vn',
-      NEXTAUTH_SECRET='wmsjUYfJFranGO7muCrJfdb+M5gK3iMENEMCrW+n20M',
+      NEXTAUTH_URL:'https://ktravel.io.vn',
+      NEXTAUTH_SECRET:'wmsjUYfJFranGO7muCrJfdb+M5gK3iMENEMCrW+n20M',
 
-      NEXT_PUBLIC_NAME='ktravel.io.vn',
-      NEXT_PUBLIC_API_URL='https://admin.ktravel.io.vn',
-      NEXT_PUBLIC_URL='https://ktravel.io.vn',
+      NEXT_PUBLIC_NAME:'ktravel.io.vn',
+      NEXT_PUBLIC_API_URL:'https://admin.ktravel.io.vn',
+      NEXT_PUBLIC_URL:'https://ktravel.io.vn',
 
-      GOOGLE_CLIENT_ID='880076609170-g03thcmhcj24bc3uusi6tudir7gh6hmv.apps.googleusercontent.com',
-      GOOGLE_CLIENT_SECRET='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'
+      GOOGLE_CLIENT_ID:'880076609170-g03thcmhcj24bc3uusi6tudir7gh6hmv.apps.googleusercontent.com',
+      GOOGLE_CLIENT_SECRET:'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'
   },
   
   reactStrictMode: false,
