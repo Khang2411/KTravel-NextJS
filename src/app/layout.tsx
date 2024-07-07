45d926e3de3fa7704d2f057ec0cc24e1e5e896d0
Khang2411 <46453014+Khang2411@users.noreply.github.com>
2024-07-07 09:13:37 +0700

    Update layout.tsx runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import type { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin'] })
 
 export const maxDuration = 60;
+export const runtime = 'edge'
 
 export const metadata: Metadata = {
   title: 'Du lịch | Nhà nghỉ dưỡng Ktravel',
