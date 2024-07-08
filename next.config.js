099d651d0bc399051b8aca2d7a73192d7cea9548
Khang2411 <hk24112000@gmail.com>
2024-07-08 10:02:18 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  httpAgentOptions: {
+    keepAlive: false,
+  },
   experimental: {
     serverActions: true,
   },
